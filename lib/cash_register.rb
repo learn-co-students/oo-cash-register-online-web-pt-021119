881842234799f34e3fdcb87dcc8042178748d850
Henry Sztanski <henry@sztanski.com>
2019-03-12 02:26:25 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,19 +1,31 @@
+require "pry"
 
 class CashRegister
   attr_accessor :total, :discount
+  attr_reader :items
 
   def initialize(discount = 0)
     @discount = discount
-    @total = 0
+    @total = 0.0
+    @items = []
+
   end
 
-  def add_item(title, price)
+  def add_item(title, price, quantity = 1)
     # accepts a title and a price and increases the total (FAILED - 1)
     # also accepts an optional quantity (FAILED - 2)
-    # doesn't forget about the previous total (FAILED - 3)
+    @previos_total = self.total
+    self.total += price * quantity
+    quantity.times {@items << title}
   end
 
   def apply_discount
+    if self.discount != 0
+      self.total *= (1-self.discount.to_f/100)
+      "After the discount, the total comes to $#{self.total.to_i}."
+    else
+      "There is no discount to apply."
+    end
     # the cash register was initialized with an employee discount
     #   applies the discount to the total price (FAILED - 4)
     #   returns success message with updated total (FAILED - 5)
@@ -22,11 +34,8 @@ class CashRegister
     #   returns a string error message that there is no discount to apply (FAILED - 7)
   end
 
-  def items
-    # returns an array containing all items that have been added (FAILED - 8)
-  end
-
   def void_last_transaction
-    # subtracts the last transaction from the total (FAILED - 9)
+    binding.pry
+    self.total -= @previous_total
   end
 end
