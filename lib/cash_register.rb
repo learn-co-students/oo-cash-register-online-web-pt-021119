6c626994114e7eac8ac5f8a377e8b11586b8e11a
Katie Blizzard <kblizzard11@gmail.com>
2019-03-16 20:13:52 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,36 +1,35 @@
 class CashRegister 
   
-attr_accessor :total, :discount, :items 
-
-
-
-def initialize(discount = 0) 
-  @total = 0 
-  @discount = discount
-  @items = []
-  @transactions = []
-end 
-
-def add_item(item, price, quantity = 1)
-  @total += (price*quantity)
-  quantity.times do 
-    @items.push(item) 
+  attr_accessor :total, :discount
+  attr_reader :items, :transactions 
+  
+  def initialize(discount = 0) 
+    @total = 0 
+    @discount = discount
+    @items = []
+    @transactions = []
   end 
-  @transactions.push(price.to_f*quantity)
-end 
-
-def apply_discount 
-  if @discount > 0 
-    @total -= @total*(@discount.to_f/100) 
-    return "After the discount, the total comes to $#{@total.to_i}."
-  else 
-    return "There is no discount to apply."
+  
+  def add_item(item, price, quantity = 1)
+    self.total += (price*quantity)
+    quantity.times do 
+      @items.push(item) 
+    end 
+    @transactions.push(price*quantity)
   end 
-end 
-
-def void_last_transaction 
-  @total -= @transactions.last 
-end 
+  
+  def apply_discount 
+    if @discount > 0 
+      @total -= @total*(@discount.to_f/100) 
+      return "After the discount, the total comes to $#{@total.to_i}."
+    else 
+      return "There is no discount to apply."
+    end 
+  end 
+  
+  def void_last_transaction 
+    @total -= @transactions.last 
+  end 
 
 
 
